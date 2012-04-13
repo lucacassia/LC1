b995c7f56305b397e223639821032d61ed99a8f9
megadoro <6.626068h@gmail.com>
2012-04-13 17:58:37 +0200

    update

--- include/metro2.h
+++ include/metro2.h
@@ -10,10 +10,10 @@ void metro2(void){
 	fprintf(pipe, "set style line 1 lc rgb '#228f26' lt 1 lw 2 pt 7 ps 1.5 # --- green\n");
 	fprintf(pipe, "set grid\n");
 	fprintf(pipe, "set title \"Metropolis\"\n");
-	fprintf(pipe, "set xlabel \"i\"\n");
-	fprintf(pipe, "set ylabel \"o[i]\"\n");
+	fprintf(pipe, "set xlabel \"dt\"\n");
+	fprintf(pipe, "set ylabel \"o[dt]\"\n");
 	fprintf(pipe, "unset key\n");
-	fprintf(pipe, "set log y\n");
+//	fprintf(pipe, "set log y\n");
 	fprintf(pipe, "set term postscript enhanced color landscape lw 1 \"Verdana,10\"\n");
 	fprintf(pipe, "set output 'metro2.eps'\n");
 	fprintf(pipe, "plot 'metro2.dat' with linespoints ls 1\n");
--- main/metropolis.c
+++ main/metropolis.c
@@ -7,14 +7,15 @@
 #include"metro3.h"
 #include"libutil.h"
 
-#define N 16
+#define N 32
 #define Pi 3.14159265
 #define POINTS 100
+#define BIN_W 100
 
-double M=10;
-double W=2*Pi/(N-1);
+double M=1;
+double W=1;
 double D=3;
-double x[N],o[N];
+double x[N],o[N],tmp[N],var[N];
 
 double V(double x){ return M*W*W*x*x/2; }
 
@@ -49,7 +50,7 @@ void ooo(double* x,double* o){
     int dt; for(dt=0;dt<N;dt++){
         double odt=0;
         int i; for(i=0;i<N;i++)
-            odt+=x[i]*x[i+dt];
+            odt+=x[i]*x[(i+dt)%N];
         o[dt]+=odt/N;
     }
 }
@@ -58,39 +59,48 @@ int main(int argc,char* argv[]){
     /*init cycles*/
     int cycles=1e6; if(argc==2) cycles=atoi(argv[1]);
     /*init state*/
-    int i;for(i=0;i<N;i++){x[i]=0;o[i]=0;}
+    int i,dt;for(i=0;i<N;i++){x[i]=o[i]=tmp[i]=var[i]=0;}
     /*init ranlux*/
     rlxd_init(2,time(NULL));
-    /*init action*/
-    double S=action(x,N);
 
-    /*metropolis loop*/
+    /*action*/
+    double S=action(x,N);
     FILE *f3 = fopen("metro3.dat","w");
-    for(i=0;i<cycles;i++){
+    for(i=0;i<1000;i++){
         S+=metropolis(x);
-        if(i>500) ooo(x,o);
-        if(i<1000) fprintf(f3,"%d\t%lf\n",(i+1),S);
-        loading(i,cycles);
+        fprintf(f3,"%d\t%lf\n",(i+1),S);
     }
     fclose(f3);
+
+    /*metropolis loop*/
+    for(i=0;i<cycles;i++){
+        metropolis(x);
+        ooo(x,tmp);
+        if((i+1)%BIN_W==0)
+            for(dt=0;dt<N;dt++){
+                o[dt]+=tmp[dt]/BIN_W;
+                var[dt]+=tmp[dt]*tmp[dt]/BIN_W/BIN_W;
+                tmp[dt]=0;
+            }
+        loading(i,cycles);
+    }
 
     /*print data to file*/
     FILE *f1 = fopen("metro1.dat","w");
     FILE *f2 = fopen("metro2.dat","w");
     for(i=0;i<N;i++){
-        o[i]/=(cycles-500);
+        o[i]/=cycles/BIN_W;
+        var[i]=var[i]/(cycles/BIN_W)-o[i]*o[i];
         fprintf(f1,"%d\t%lf\n",i,x[i]);
-        fprintf(f2,"%d\t%lf\n",i,o[i]);
+        fprintf(f2,"%d\t%lf\t%lf\n",i,fabs(o[i]),var[i]);
     }
     fclose(f1);
     fclose(f2);
 
     /*plot data from file*/
-    metro1();
-    metro2();
-    metro3();
+    metro1();metro2();metro3();
     system("rm metro1.dat");
-    system("rm metro2.dat");
+//    system("rm metro2.dat");
     system("rm metro3.dat");
     printf("\n");
     return 0;
