4323977d3068424c602b9e815ea5aa60f0370df9
megadoro <6.626068h@gmail.com>
2012-04-19 20:18:27 +0200

    update

--- main/metropolis.c
+++ main/metropolis.c
@@ -2,6 +2,7 @@
 #include<math.h>
 #include<time.h>
 #include"random.h"
+#include"metro0.h"
 #include"metro1.h"
 #include"metro2.h"
 #include"metro3.h"
@@ -10,21 +11,20 @@
 #define N 32
 #define BIN_W 100
 
-double M=1;
-double W=1;
-double D=3;
-double x[N],o[N],tmp[N],var[N],gmm[30];
+double M = 1;
+double W = 1;
+double D = 3;
+double x[N],o[N],tmp[N],var[N];
+double *data;
 
 double V(double x){ return M*W*W*x*x/2; }
 
-double lagrangian(double x1,double x2){ return M*(x2-x1)*(x2-x1)/2+(V(x1)+V(x2))/2; }
-
 double dS(double *x,double y,int i){ return M*((x[i]-y)*(x[(i+1)%N]+x[(N+i-1)%N])+(y*y-x[i]*x[i]))+V(y)-V(x[i]); }
 
-double action(const double *x,int n){
+double action(const double *x){
     double s = 0;
-    int i; for(i = 0; i < n-1; i++)
-        s += lagrangian(x[i],x[i+1]);
+    int i; for(i = 0; i < N-1; i++)
+        s += M*(x[i+1]-x[i])*(x[i+1]-x[i])/2+(V(x[i])+V(x[i+1]))/2;
     return s;
 }
 
@@ -42,62 +42,64 @@ double metropolis(double *x){
     return DS;
 }
 
-void ooo(double* x,double* o){
+void ooo(double* x,double* o,double* data){
     int dt; for(dt = 0; dt < N; dt++){
         double odt = 0;
         int i; for(i = 0; i < N; i++)
             odt += x[i]*x[(i+dt)%N];
         o[dt] += odt/N;
+        data[dt] = odt/N;
     }
 }
 
-/*void selfCorrelation(double* x){
-    int t; for(t = 0; t < 30-t; t++){
-        int i; for(i = 0; i < cycles-t; i++)
-            
+double autoCorrelation(int i,int k,int n){
+    double mean,var,Rk; mean = var = Rk = 0;
+    int t; for(t = 0; t < n-k; t++){
+        Rk += data[N*t+i]*data[N*(t+k)+i];
+        mean += data[N*t+i];
+        var += data[N*t+i]*data[N*t+i];
     }
-}*/
+    mean /= n-k;
+    var = var/(n-k)-mean*mean;
+    return (Rk/(n-k)-mean*mean)/var;
+}
 
 int main(int argc,char* argv[]){
     /*init cycles*/
     int cycles = 1e6; if(argc == 2) cycles = atoi(argv[1]);
-    /*init state*/
-    int i,dt;for(i = 0; i < N; i++){
-        x[i] = 100;
-        o[i] = tmp[i] = var[i] = gmm[i] = 0;
+    /*init mem*/
+    data = (double*)malloc(N*cycles*sizeof(double));
+    /*init variables*/
+    int i; for(i = 0; i < N; i++){
+        x[i] = 100; /*hot action*/
+        o[i] = tmp[i] = var[i] = 0;
     }
+
     /*init ranlux*/
     rlxd_init(2,time(NULL));
 
     /*action*/
-    double S = action(x,N);
-    FILE *f3 = fopen("metro3.dat","w");
-    for(i = 0; i < 1000; i++){
-        S += metropolis(x);
-        fprintf(f3,"%d\t%lf\n",(i+1),S);
-    }
-    fclose(f3);
+    double S = action(x);
+    FILE *f0 = fopen("metro0.dat","w");
+    for(i = 0; i < 1000; i++)
+        fprintf(f0,"%d\t%lf\n",(i+1),S += metropolis(x));
+    fclose(f0); metro0(); system("rm metro0.dat");
 
-    FILE *f4 = fopen("metro4.dat","w");
     /*metropolis loop*/
     for(i = 0; i < cycles; i++){
         metropolis(x);
-        ooo(x,tmp);
+        ooo(x,tmp,&data[i*N]);
         if((i+1)%BIN_W == 0){
-            fprintf(f4,"%d",(i+1)/BIN_W);           /*print*/
-            for(dt = 0; dt < N; dt++){
+            int dt; for(dt = 0; dt < N; dt++){
                 o[dt] += tmp[dt]/BIN_W;
-                fprintf(f4,"\t%lf",tmp[dt]/BIN_W);  /*print*/
                 var[dt] += tmp[dt]*tmp[dt]/BIN_W/BIN_W;
                 tmp[dt] = 0;
             }
-            fprintf(f4,"\n");                       /*print*/
         }
         loading(i,cycles);
     }
-    fclose(f4);
 
-    /*print data to file*/
+    /*print data to file and plot*/
     FILE *f1 = fopen("metro1.dat","w");
     FILE *f2 = fopen("metro2.dat","w");
     for(i = 0; i < N; i++){
@@ -106,14 +108,21 @@ int main(int argc,char* argv[]){
         fprintf(f1,"%d\t%lf\n",i,x[i]);
         fprintf(f2,"%d\t%lf\t%lf\n",i,fabs(o[i]),var[i]);
     }
-    fclose(f1);
-    fclose(f2);
+    fclose(f1);metro1(); system("rm metro1.dat");
+    fclose(f2);metro2(); system("rm metro2.dat");
 
-    /*plot data from file*/
-    metro1();metro2();metro3();
-    system("rm metro1.dat");
-    system("rm metro2.dat");
-    system("rm metro3.dat");
-    printf("\n");
+    /*autocorrelation*/
+    FILE *f3 = fopen("metro3.dat","w");
+    for(i = 0; i < 30; i++)
+        fprintf(f3,"%d\t%lf\n",i,autoCorrelation(1,i,cycles));
+    fclose(f3); metro3(); system("rm metro3.dat");
+
+    /*delta E*/
+    double dE = 0;
+    for(i = 1; i < 4; i++)
+        dE += acosh((o[i+1]+o[i-1])/(2*o[i]));
+    printf("\n\n dE = %lf\n\n",dE/=3);
+
+    free(data);
     return 0;
 }
