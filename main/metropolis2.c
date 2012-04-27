d22e220c45746696b4dde0e674b4b648bb662a3b
megadoro <6.626068h@gmail.com>
2012-04-27 17:22:50 +0200

    update

--- main/metropolis2.c
+++ main/metropolis2.c
@@ -1,26 +1,23 @@
+#include<stdlib.h>
+#include<stdio.h>
 #include<math.h>
 #include<time.h>
+#include"libutil.h"
 #include"random.h"
 #include"plot.h"
-#include"libutil.h"
-#include"mvector.h"
 
 #define N 32
-#define PRINT
 
 double M = 1;
 double W = 1;
 double D = 3;
 
-double V(double x){ return M*W*W*x*x/2; }
+double V(double x){
+    return M*W*W*x*x/2;
+}
 
-double dS(double *x,double y,int i){ return M*((x[i]-y)*(x[(i+1)%N]+x[(i-1+N)%N])+(y*y-x[i]*x[i]))+V(y)-V(x[i]); }
-
-double action(double *x){
-    double s = 0;
-    int i; for(i = 0; i < N-1; i++)
-        s += M*(x[i+1]-x[i])*(x[i+1]-x[i])/2+(V(x[i])+V(x[i+1]))/2;
-    return s;
+double dS(double *x,double y,int i){
+    return M*((x[i]-y)*(x[(i+1)%N]+x[(i-1+N)%N])+(y*y-x[i]*x[i]))+V(y)-V(x[i]);
 }
 
 double metropolis(double *x){
@@ -44,20 +41,6 @@ double correlation(double* x,int dt){
     return odt/N;
 }
 
-double autoCorrelation(int i,int k,int n,mvector* data){
-    double mean,var,Rk;
-    mean = var = Rk = 0;
-    int t; for(t = 0; t < n-k; t++){
-        double tmp = mget(data,N*t+i);
-        Rk += tmp*mget(data,N*(t+k)+i);
-        mean += tmp;
-        var += tmp*tmp;
-    }
-    mean /= n-k;
-    var = var/(n-k)-mean*mean;
-    return (Rk/(n-k)-mean*mean)/var;
-}
-
 int main(int argc,char* argv[]){
     /*init ranlux*/
     rlxd_init(2,time(NULL));
@@ -67,84 +50,92 @@ int main(int argc,char* argv[]){
     int bin = cycles/wid;
     int i,j,k;
     /*init mem*/
-    mvector* data = mopen(0);
-    mvector* dtcl = mopen(N*bin);
+    double* dtcl = malloc(N*bin*sizeof(double));
     /*init variables*/
     double x[N],c[N],var[N],tmp;
     for(i = 0; i < N; i++){
         x[i] = 100;
         c[i] = var[i] = 0;
     }
+    FILE* f;
 
     /*action*/
-    double S = action(x);
-    FILE *f = fopen("action.dat","w");
     for(i = 0; i < 1000; i++)
-        fprintf(f,"%d\t%lf\n",(i+1),S += metropolis(x));
-    fclose(f); plot_action(); system("rm action.dat");
+        metropolis(x);
 
     /*metropolis loop*/
     for(i = 0; i < bin; i++)
         for(j = 0; j < wid; j++){
             metropolis(x);
-            for(k = 0; k < N; k++){
-                tmp = correlation(x,k);
-                madd(data,tmp);
-                mset(dtcl, k*bin+i, mget(dtcl, k*bin+i) + tmp/wid);
-            }
-#ifdef PRINT
+            for(k = 0; k < N; k++)
+                dtcl[k*bin+i] += correlation(x,k)/wid;
             loading(i*wid+j,bin*wid);
-#endif
         }
-    printf("\n");
-
-    /*autocorrelation*/
-    f = fopen("autocorrelation.dat","w");
-    for(i = 0; i < 30; i++)
-        fprintf(f,"%d\t%lf\n",i,autoCorrelation(1,i,bin*wid,data));
-    fclose(f); plot_autocorrelation(); system("rm autocorrelation.dat");
-    mclose(data);
 
     /*correlation*/
-    f = fopen("correlation.dat","w");
     for(k = 0; k < N; k++){
         for(i = 0; i < bin; i++){
-            c[k] += tmp = mget(dtcl, k*bin+i);
+            c[k] += tmp = dtcl[k*bin+i];
             var[k] += tmp*tmp;
         }
         c[k] /= bin;
         var[k] = var[k]/bin-c[k]*c[k];
-        fprintf(f,"%d\t%lf\t%lf\n",k,fabs(c[k]),var[k]);
     }
-    fclose(f); plot_correlation(); system("rm correlation.dat");
 
-    /*delta E*/
-    double dE = 0;
-    for(i = 2; i < 5; i++)
-        dE += acosh((c[i+1]+c[i-1])/(2*c[i]));
+    /*ΔE & W*/
+    double dE = 0, W = 0;;
+    for(k = 2; k < 5; k++){
+        dE += tmp = acosh((c[k+1]+c[k-1])/(2*c[k]));
+        W += sqrt(c[k]*exp(tmp*N/2.0)/(2*cosh(tmp*(N/2.0-k))));
+    }
     dE /= 3;
+    W /= 3;
 
-    /*jackknife*/
-    double mcl[6],dEm = 0,var_dE = 0;
+    /*cluster generation*/
+    double mcl[6];
     for(i = 1; i < 6; i++){
         mcl[i] = 0;
         for(j = 0; j < bin; j++)
-            mcl[i] += mget(dtcl,i*bin+j);
+            mcl[i] += dtcl[i*bin+j];
         mcl[i] /= bin;
         for(j = 0; j < bin; j++)
-            mset(dtcl,i*bin+j,mcl[i]+(mcl[i]-mget(dtcl,i*bin+j))/(bin-1));
+            dtcl[i*bin+j] = mcl[i]+(mcl[i]-dtcl[i*bin+j])/(bin-1);
     }
-    for(i = 2; i < 5; i++)
-        dEm += acosh((mcl[i+1]+mcl[i-1])/(2*mcl[i]));
+
+    /*compute mean values*/
+    double dEm = 0, Wm = 0;
+    for(k = 2; k < 5; k++){
+        dEm += tmp = acosh((mcl[k+1]+mcl[k-1])/(2*mcl[k]));
+        Wm += sqrt(mcl[k]*exp(tmp*N/2.0)/(2*cosh(tmp*(N/2.0-k))));
+    }
+
+    /*compute variance*/
+    double var_dE = 0, var_W = 0;
     for(j = 0; j < bin; j++){
-        double tmp = 0;
-        for(i = 2; i < 5; i++)
-            tmp += acosh((mget(dtcl,(i+1)*bin+j)+mget(dtcl,(i-1)*bin+j))/(2*mget(dtcl,i*bin+j)));
-        var_dE += (tmp-dEm)*(tmp-dEm)/9;
+        double tmp1 = 0, tmp2 = 0;
+        for(k = 2; k < 5; k++){
+            tmp1 += tmp = acosh((dtcl[(k+1)*bin+j]+dtcl[(k-1)*bin+j])/(2*dtcl[k*bin+j]));
+            tmp2 += sqrt(c[k]*exp(tmp*N/2.0)/(2*cosh(tmp*(N/2.0-k))));
+        }
+        var_dE += (tmp1-dEm)*(tmp1-dEm)/9;
+        var_W += (tmp2-Wm)*(tmp2-Wm)/9;
     }
     var_dE = (var_dE*(bin-1))/bin;
-    printf("\n\n dE  = %lf\n\n σ = %e\n\n",dE,sqrt(var_dE));
+    var_W = (var_W*(bin-1))/bin;
+    free(dtcl);
 
-    mclose(dtcl);
+    /*plot ΔE*/
+    f = fopen("dE.dat","a");
+    fprintf(f,"%lf\n",dE);
+    fclose(f);
+    fit();
+
+    /*plot W*/
+    f = fopen("W.dat","a");
+    fprintf(f,"%lf\n",W);
+    fclose(f);
+
+    printf("\n\n ΔE  = %lf\n\n σ = %e\n\n W  = %lf\n\n σ = %e\n\n",dE,sqrt(var_dE),W,sqrt(var_W));
+
     return 0;
 }
