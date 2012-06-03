19b781858f59db817ade62d4d19d592f8eddfc32
megadoro <6.626068h@gmail.com>
2012-06-03 11:17:08 +0200

    update

--- include/ode.h
+++ include/ode.h
@@ -5,27 +5,30 @@
 
 #define dt (1e-3)
 
-typedef enum { ODE_CHAOTIC, ODE_VANDERPOL, ODE_LORENZ } ODE_TYPE;
+typedef enum { ODE_CHAOTIC, ODE_VANDERPOL, ODE_DUFFING, ODE_LORENZ, ODE_ROSSLER, ODE_RABINOVICH_FABRIKANT } ODE_TYPE;
 
-double chaotic(double x,double v,double t){
-    return -sin(x)-0.3*v+1.4*cos(2*t/3);
-}
+double dummy(double x,double y, double z,double t) {return 0;}
 
-double vanderpol(double x,double v,double t){
-    return 4*(1-x*x)*v-x;
-}
+double chaotic_x(double x,double y, double z,double t) {return y;}
+double chaotic_y(double x,double y, double z,double t) {return -sin(x)-0.3*y+1.4*cos(2*t/3);}
 
-double lorenz_x(double x,double y,double z){
-    return 10*(y-x);
-}
+double vanderpol_x(double x,double y, double z,double t) {return y;}
+double vanderpol_y(double x,double y, double z,double t) {return 4*(1-x*x)*y-x;}
 
-double lorenz_y(double x,double y,double z){
-    return x*(28-z)-y;
-}
+double duffing_x(double x,double y, double z,double t) {return y;}
+double duffing_y(double x,double y, double z,double t) {return 0.3*cos(t)-0.2*y+x-pow(x,3);}
 
-double lorenz_z(double x,double y,double z){
-    return x*y-8*z/3;
-}
+double lorenz_x(double x,double y,double z, double t) {return 10*(y-x);}
+double lorenz_y(double x,double y,double z, double t) {return x*(28-z)-y;}
+double lorenz_z(double x,double y,double z, double t) {return x*y-8*z/3;}
+
+double rossler_x(double x,double y,double z, double t) {return -y-z;}
+double rossler_y(double x,double y,double z, double t) {return x+0.1*y;}
+double rossler_z(double x,double y,double z, double t) {return 0.1+z*(x-14);}
+
+double rabinovich_fabrikant_x(double x,double y,double z, double t) {return y*(z-1+pow(x,2))+0.87*x;}
+double rabinovich_fabrikant_y(double x,double y,double z, double t) {return x*(3*z+1-pow(x,2))+0.87*y;}
+double rabinovich_fabrikant_z(double x,double y,double z, double t) {return -2*z*(1.1+x*y);}
 
 typedef struct _plist{
     double x,y,z,t;
@@ -64,41 +67,60 @@ void plist_erase(plist **head){
 
 void plist_evolve_ode(plist **head_ptr, plist **tail_ptr, ODE_TYPE type){
     double x = 0, y = 0, z = 0, k1, k2, k3, k4;
-    if(type == ODE_CHAOTIC){
-        k1 = chaotic((*head_ptr)->x,(*head_ptr)->y,(*head_ptr)->t)*dt;
-        k2 = chaotic((*head_ptr)->x,(*head_ptr)->y+k1/2,(*head_ptr)->t+dt/2)*dt;
-        k3 = chaotic((*head_ptr)->x,(*head_ptr)->y+k2/2,(*head_ptr)->t+dt/2)*dt;
-        k4 = chaotic((*head_ptr)->x,(*head_ptr)->y+k3,(*head_ptr)->t+dt)*dt;
-        y = (*head_ptr)->y + k1/6+k2/3+k3/3+k4/6;
-        x = (*head_ptr)->x + (*head_ptr)->y * dt;
+    double (*function_x)(double,double,double,double) = NULL;
+    double (*function_y)(double,double,double,double) = NULL;
+    double (*function_z)(double,double,double,double) = NULL;
+    switch(type){
+        case ODE_CHAOTIC:
+            function_x = chaotic_x;
+            function_y = chaotic_y;
+            function_z = dummy;
+            break;
+        case ODE_VANDERPOL:
+            function_x = vanderpol_x;
+            function_y = vanderpol_y;
+            function_z = dummy;
+            break;
+        case ODE_DUFFING:
+            function_x = duffing_x;
+            function_y = duffing_y;
+            function_z = dummy;
+            break;
+        case ODE_LORENZ:
+            function_x = lorenz_x;
+            function_y = lorenz_y;
+            function_z = lorenz_z;
+            break;
+        case ODE_ROSSLER:
+            function_x = rossler_x;
+            function_y = rossler_y;
+            function_z = rossler_z;
+            break;
+        case ODE_RABINOVICH_FABRIKANT:
+            function_x = rabinovich_fabrikant_x;
+            function_y = rabinovich_fabrikant_y;
+            function_z = rabinovich_fabrikant_z;
+            break;
     }
-    if(type == ODE_VANDERPOL){
-        k1 = vanderpol((*head_ptr)->x,(*head_ptr)->y,(*head_ptr)->t)*dt;
-        k2 = vanderpol((*head_ptr)->x,(*head_ptr)->y+k1/2,(*head_ptr)->t+dt/2)*dt;
-        k3 = vanderpol((*head_ptr)->x,(*head_ptr)->y+k2/2,(*head_ptr)->t+dt/2)*dt;
-        k4 = vanderpol((*head_ptr)->x,(*head_ptr)->y+k3,(*head_ptr)->t+dt)*dt;
-        y = (*head_ptr)->y + k1/6+k2/3+k3/3+k4/6;
-        x = (*head_ptr)->x + (*head_ptr)->y * dt;
-    }
-    if(type == ODE_LORENZ){
-        k1 = lorenz_x((*head_ptr)->x,(*head_ptr)->y,(*head_ptr)->z)*dt;
-        k2 = lorenz_x((*head_ptr)->x+k1/2,(*head_ptr)->y,(*head_ptr)->z)*dt;
-        k3 = lorenz_x((*head_ptr)->x+k2/2,(*head_ptr)->y,(*head_ptr)->z)*dt;
-        k4 = lorenz_x((*head_ptr)->x+k3,(*head_ptr)->y,(*head_ptr)->z)*dt;
-        x = (*head_ptr)->x + k1/6+k2/3+k3/3+k4/6;
 
-        k1 = lorenz_y((*head_ptr)->x,(*head_ptr)->y,(*head_ptr)->z)*dt;
-        k2 = lorenz_y((*head_ptr)->x,(*head_ptr)->y+k1/2,(*head_ptr)->z)*dt;
-        k3 = lorenz_y((*head_ptr)->x,(*head_ptr)->y+k2/2,(*head_ptr)->z)*dt;
-        k4 = lorenz_y((*head_ptr)->x,(*head_ptr)->y+k3,(*head_ptr)->z)*dt;
-        y = (*head_ptr)->y + k1/6+k2/3+k3/3+k4/6;
+    k1 = function_x((*head_ptr)->x,(*head_ptr)->y,(*head_ptr)->z,(*head_ptr)->t)*dt;
+    k2 = function_x((*head_ptr)->x+k1/2,(*head_ptr)->y,(*head_ptr)->z,(*head_ptr)->t+dt/2)*dt;
+    k3 = function_x((*head_ptr)->x+k2/2,(*head_ptr)->y,(*head_ptr)->z,(*head_ptr)->t+dt/2)*dt;
+    k4 = function_x((*head_ptr)->x+k3,(*head_ptr)->y,(*head_ptr)->z,(*head_ptr)->t+dt)*dt;
+    x = (*head_ptr)->x + k1/6+k2/3+k3/3+k4/6;
 
-        k1 = lorenz_z((*head_ptr)->x,(*head_ptr)->y,(*head_ptr)->z)*dt;
-        k2 = lorenz_z((*head_ptr)->x,(*head_ptr)->y,(*head_ptr)->z)*dt;
-        k3 = lorenz_z((*head_ptr)->x,(*head_ptr)->y,(*head_ptr)->z)*dt;
-        k4 = lorenz_z((*head_ptr)->x,(*head_ptr)->y,(*head_ptr)->z+k3)*dt;
-        z = (*head_ptr)->z + k1/6+k2/3+k3/3+k4/6;
-    }
+    k1 = function_y((*head_ptr)->x,(*head_ptr)->y,(*head_ptr)->z,(*head_ptr)->t)*dt;
+    k2 = function_y((*head_ptr)->x,(*head_ptr)->y+k1/2,(*head_ptr)->z,(*head_ptr)->t+dt/2)*dt;
+    k3 = function_y((*head_ptr)->x,(*head_ptr)->y+k2/2,(*head_ptr)->z,(*head_ptr)->t+dt/2)*dt;
+    k4 = function_y((*head_ptr)->x,(*head_ptr)->y+k3,(*head_ptr)->z,(*head_ptr)->t+dt)*dt;
+    y = (*head_ptr)->y + k1/6+k2/3+k3/3+k4/6;
+
+    k1 = function_z((*head_ptr)->x,(*head_ptr)->y,(*head_ptr)->z,(*head_ptr)->t)*dt;
+    k2 = function_z((*head_ptr)->x,(*head_ptr)->y,(*head_ptr)->z+k1/2,(*head_ptr)->t+dt/2)*dt;
+    k3 = function_z((*head_ptr)->x,(*head_ptr)->y,(*head_ptr)->z+k2/2,(*head_ptr)->t+dt/2)*dt;
+    k4 = function_z((*head_ptr)->x,(*head_ptr)->y,(*head_ptr)->z+k3,(*head_ptr)->t+dt)*dt;
+    z = (*head_ptr)->z + k1/6+k2/3+k3/3+k4/6;
+
     plist_add_front(head_ptr, x, y, z, (*head_ptr)->t+dt);
 }
 
